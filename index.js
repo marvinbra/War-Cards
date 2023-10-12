5811e720a3cc5170b536d029d74afa5173a68581
marvinbra <marvinbopp37@gmail.com>
2023-10-12 21:25:08 +0200

    display remaining cards
    js

--- index.js
+++ index.js
@@ -16,8 +16,10 @@ document.getElementById("draw-cards").addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.cards)
-    
+            console.log(data.remaining)
+            remainingCards.innerHTML = `
+                <p>Remaining Cards: ${data.remaining}</p>
+            `
             document.getElementById("card-slot1").innerHTML = 
             `<img src=${data.cards[0].image} class="card" />`
              document.getElementById("card-slot2").innerHTML = 
